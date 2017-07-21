d144761da57f51a04aa3cb8b4a0466965fb78da7
Goran S. Milovanović <goran.s.milovanovic@gmail.com>
2017-07-21 16:03:19 +0200

    Beta v0.1

---
+++ WDCM_Sqoop_Clients.R
@@ -0,0 +1,127 @@
+
+### ---------------------------------------------------------------------------
+### --- WDCM Search Module, v. Beta 0.1
+### --- Script: WDCM_Sqoop_Clients.R, v. Beta 0.1
+### --- Author: Goran S. Milovanovic, Data Analyst, WMDE
+### --- Developed under the contract between Goran Milovanovic PR Data Kolektiv 
+### --- and WMDE.
+### --- Contact: goran.milovanovic_ext@wikimedia.de
+### ---------------------------------------------------------------------------
+### --- DESCRIPTION:
+### --- WDCM_Sqoop_Clients.R takes a list of client projects
+### --- that maintain the wbc_entity_usage tables
+### --- and sqoopes these tables into a single Hadoop Avro file
+### --- on production (currently: stat1002.eqiad.wmnet).
+### ---------------------------------------------------------------------------
+### --- OUTPUT: 
+### --- Results are stored on the Hadoop cluster
+### --- database: goransm
+### --- directory: wdcmsqoop
+### --- table: wdcm_clients_wb_entity_usage
+### ---------------------------------------------------------------------------
+### --- RUN:
+### --- nohup Rscript /home/goransm/RScripts/WDCM_R/WDCM_Sqoop_Clients.R &
+### ---------------------------------------------------------------------------
+
+### --- Setup
+rm(list = ls())
+
+### --- Collect all client projects that maintain wbc_entitiy_usage
+# - show all databases
+mySqlArgs <- 
+  '--defaults-file=/etc/mysql/conf.d/analytics-research-client.cnf -h analytics-store.eqiad.wmnet -A'
+mySqlInput <- '"SHOW DATABASES;" > /home/goransm/RScripts/WDCM_R/wdcmAuxDir/databases.tsv'
+# - command:
+mySqlCommand <- paste0("mysql ", mySqlArgs, " -e ", mySqlInput, collapse = "")
+system(command = mySqlCommand, wait = TRUE)
+# - get databases
+setwd('/home/goransm/RScripts/WDCM_R/wdcmAuxDir')
+clients <- read.table('databases.tsv', header = T, check.names = F, stringsAsFactors = F, sep = "\t")
+# - select client projects
+wClients <- which(grepl("wiki$|books$|voyage$|source$|quote$|wiktionary$|news$|media$", clients$Database))
+clients <- clients$Database[wClients]
+# - look-up for wbc_entity_usage tables
+projectsTracking <- character()
+for (i in 1:length(clients)) {
+  # - show tables from the i-th client project
+  mySqlArgs <- 
+    '--defaults-file=/etc/mysql/conf.d/analytics-research-client.cnf -h analytics-store.eqiad.wmnet -A'
+  mySqlInput1 <- paste('"USE ', clients[i], ';', sep = "")
+  mySqlInput2 <- 'SHOW TABLES;\" > /home/goransm/RScripts/WDCM_R/wdcmAuxDir/clienttables.tsv'
+  mySqlInput <- paste(mySqlInput1, mySqlInput2, sep = "")
+  # - command:
+  mySqlCommand <- paste0("mysql ", mySqlArgs, " -e ", mySqlInput, collapse = "")
+  system(command = mySqlCommand, wait = TRUE)
+  tables <- read.table('clienttables.tsv', header = T, check.names = F, stringsAsFactors = F, sep = "\t")
+  if("wbc_entity_usage" %in% tables[, 1]) {
+    projectsTracking <- append(projectsTracking, clients[i]) 
+  }
+}
+# - store projectsTracking
+write.csv(projectsTracking, "projectsTracking.csv")
+
+### --- Sqoop all client projects that maintain wbc_entitiy_usage
+wdcmSqoopReport <- data.frame(project = projectsTracking,
+                              startTime = character(length(projectsTracking)),
+                              endTime = character(length(projectsTracking)),
+                              stringsAsFactors = F
+                              )
+for (i in 1:length(projectsTracking)) {
+  wdcmSqoopReport$project[i] <- projectsTracking[i]
+  wdcmSqoopReport$startTime[i] <- as.character(Sys.time())
+  # - sqoop command:
+  sqoopCommand <- paste("sqoop import --connect jdbc:mysql://analytics-store.eqiad.wmnet/",
+                        projectsTracking[i],
+                        ' --password-file /user/goransm/mysql-analytics-research-client-pw.txt --username research -m 4 ',
+                        '--query "select * from wbc_entity_usage where \\$CONDITIONS" --split-by eu_row_id --as-avrodatafile --target-dir /user/goransm/wdcmsqoop/wdcm_clients_wb_entity_usage/wiki_db=',
+                        projectsTracking[i],
+                        ' --delete-target-dir',
+                        sep = ""
+                        )
+  system(command = sqoopCommand, wait = TRUE)
+  # - create Hive table if this is the first entry:
+  if (i == 1) {
+    hiveCommand <- "\"USE goransm; CREATE EXTERNAL TABLE \\\`goransm.wdcm_clients_wb_entity_usage\\\`(
+                        \\\`eu_row_id\\\`           bigint      COMMENT '',
+                        \\\`eu_entity_id\\\`        string      COMMENT '',
+                        \\\`eu_aspect\\\`           string      COMMENT '',
+                        \\\`eu_page_id\\\`          bigint      COMMENT '',
+                        \\\`eu_touched\\\`          string      COMMENT ''
+                      )
+                      COMMENT
+                        ''
+                      PARTITIONED BY (
+                        \\\`wiki_db\\\` string COMMENT 'The wiki_db project')
+                      ROW FORMAT SERDE
+                        'org.apache.hadoop.hive.serde2.avro.AvroSerDe'
+                      STORED AS INPUTFORMAT
+                        'org.apache.hadoop.hive.ql.io.avro.AvroContainerInputFormat'
+                      OUTPUTFORMAT
+                        'org.apache.hadoop.hive.ql.io.avro.AvroContainerOutputFormat'
+                      LOCATION
+                        'hdfs://analytics-hadoop/user/goransm/wdcmsqoop/wdcm_clients_wb_entity_usage';\""
+    hiveCommand <- paste("beeline -e ", hiveCommand, sep = "")
+    system(command = hiveCommand, wait = TRUE)
+  }
+  # - repair partitions:
+  system(command = 'beeline -e "USE goransm; SET hive.mapred.mode = nonstrict; MSCK REPAIR TABLE wdcm_clients_wb_entity_usage;"', wait = TRUE)
+
+  # - REPORT
+  wdcmSqoopReport$endTime[i] <- as.character(Sys.time())
+  print("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
+  print("--------------------------------------------------------------------------------")
+  print(paste("FINISHED PROJECT:", i, sep = " "))
+  print("--------------------------------------------------------------------------------")
+  print("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
+  
+}
+
+# - save wdcmSqoopReport
+fileName <- paste("wdcmSqoopReport_", Sys.time(), ".csv", sep = "")
+write.csv(wdcmSqoopReport, fileName)
+
+
+
+
+
+

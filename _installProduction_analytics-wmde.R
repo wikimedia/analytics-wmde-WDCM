469fe32ba7289291d87505d776e75791ddc3e86e
GoranMilovanovic <goran.s.milovanovic@gmail.com>
2017-11-13 22:08:40 +0100

    Installation script mkdirs r-library
    Change-Id: I23aec808dc0d1eee4beb9085c7099a218f4035b1

--- _installProduction_analytics-wmde.R
+++ _installProduction_analytics-wmde.R
@@ -8,8 +8,30 @@ Sys.setenv(
   http_proxy = "http://webproxy.eqiad.wmnet:8080",
   https_proxy = "http://webproxy.eqiad.wmnet:8080")
 
+
+# - fPath: where the scripts is run from?
+fPath <- as.character(commandArgs(trailingOnly = FALSE)[4])
+fPath <- gsub("--file=", "", fPath, fixed = T)
+fPath <- unlist(strsplit(fPath, split = "/", fixed = T))
+fPath <- paste(
+  paste(fPath[1:length(fPath) - 1], collapse = "/"),
+  "/",
+  sep = "")
+
+
+# - find out whether the fPath/r-library directory exists
+# - YES: delete it and mkdir, NO: mkdir only
+if (dir.exists(paths = paste(fPath, "r-library", sep = ""))) {
+  unlink(x = paste(fPath, "r-library", sep = ""), 
+         recursive = T)
+  dir.create(path = paste(fPath, "r-library", sep = ""))
+} else {
+  dir.create(path = paste(fPath, "r-library", sep = ""))
+}
+
 # - install WDCM related packages:
 install.packages(c("dplyr", "httr", "stringr", "XML", "readr", 
                    "data.table", "tidyr", "maptpx", "Rtsne"),
-                 lib = "/srv/analytics-wmde/r-library",
+                 lib = paste(fPath, "r-library", sep = ""),
                  repos = c(CRAN = "https://www.stats.bris.ac.uk/R/"))
+
